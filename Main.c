403aa656d1a8c6c3020aa8df174e3c5a9b2f0296
Vishal Verma <109545527+Theangryysoul@users.noreply.github.com>
2025-07-04 18:34:04 +0530

    Optimized for Invalid Input

--- Main.c
+++ Main.c
@@ -1,27 +1,42 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
-// Author : Vishal Verma game inspired from @CODEWITHHARRY
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main(){
-    srand(time(0)); // this function is use to generate a random number sequence every time
+// Author: Vishal Verma | Enhanced to handle invalid input inspired from @CodewithHarry
 
-    int randomnumber = (rand()%100)+1;
+int main() {
+    srand(time(0)); // Seed the random number generator
+
+    int randomnumber = (rand() % 100) + 1;
     int guessed_num;
     int number_of_guess = 0;
-    do
-    {
-        printf("Guess a number ");
-        scanf("%d",&guessed_num);
-        if (guessed_num>randomnumber)
-        {
-            printf("Think of a Lower number\n");
+
+    printf("=== Welcome to the Number Guessing Game ===\n");
+    printf("Guess the number between 1 and 100\n");
+
+    while (1) {
+        printf("Guess a number: ");
+
+        // Check if input is a valid integer
+        if (scanf("%d", &guessed_num) != 1) {
+            printf("Invalid input! Please enter a valid number.\n");
+
+            // Clear invalid input from the buffer
+            while (getchar() != '\n');
+            continue;
         }
-        else if(guessed_num<randomnumber){
-            printf("Think of a Higher number\n");
-        }
+
         number_of_guess++;
-    } while (guessed_num != randomnumber);
-    printf("you guessed the number in %d guesses", number_of_guess);
+
+        if (guessed_num > randomnumber) {
+            printf("Think of a lower number.\n");
+        } else if (guessed_num < randomnumber) {
+            printf("Think of a higher number.\n");
+        } else {
+            printf("You guessed the number in %d guesses!\n", number_of_guess);
+            break;
+        }
+    }
+
     return 0;
 }
